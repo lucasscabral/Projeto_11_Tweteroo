bb05a377588326f60fb178823dca5727e7fa8ad8
Lucas Cabral <lucas.9cabral41@gmail.com>
2022-06-18 13:16:51 -0300

    Finalizando o a requisição get da API

--- server.js
+++ server.js
@@ -2,20 +2,36 @@ import express from 'express'
 import cors from 'cors'
 
 const server = express()
-server.use(express.json());
+server.use(express.json())
 server.use(cors())
 const usuarios = []
+const tweets = []
 
 server.post('/sign-up', (req, res) => {
-  console.log(req.body)
-  const user = req.body.username
-  const avatar = req.body.avatar
-  const dadosUser = {
-    username: user,
-    avatar
-  }
-  console.log(dadosUser)
+  const dadosUser = req.body
   usuarios.push(dadosUser)
   res.send(usuarios)
 })
-server.listen(5001)
+server.post('/tweets', (req, res) => {
+  const tweet = req.body
+  const usuarioAtivo = usuarios.find(user => user.username === tweet.username)
+  if (usuarioAtivo === undefined) {
+    res.send('Não tem ninguem cadastrado com esse nome')
+  } else if (usuarioAtivo.username === tweet.username) {
+    const dadosTweet = {
+      username: tweet.username,
+      avatar: usuarioAtivo.avatar,
+      tweet: tweet.tweet
+    }
+    tweets.push(dadosTweet)
+    res.send(tweets)
+  }
+})
+server.get('/tweets', (req, res) => {
+  if (tweets.length === 0) {
+    res.send('')
+  } else {
+    res.send(tweets)
+  }
+})
+server.listen(5000)
